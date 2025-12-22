dea64a47589983c07d586c3589cbc35bde38aca1
varadbacchewar32 <varadbacchewar32@gmail.com>
2025-12-22 19:21:45 +0530

    Added templates and routes

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/home')
-def hello():
-    a=0
-    return 1/a
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+    
+@app.route('/inventory')
+def inventory():
+    return render_template("inventory.html")
+
+@app.route('/product')
+def product():
+    return render_template("product.html")
+
+
+@app.route('/bill')
+def bill():
+    return render_template("bill.html")
+
+@app.route('/owner')
+def owner():
+    return render_template("owner.html")
+
+@app.route('/navbar')
+def navbar():
+    return render_template("navbar.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
